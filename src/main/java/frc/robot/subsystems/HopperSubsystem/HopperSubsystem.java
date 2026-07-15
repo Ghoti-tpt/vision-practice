20c0c19c5fdd06dfeb6fddafb881cf7c7458b3b8
pie850 <164816080+pie850@users.noreply.github.com>
2026-07-15 11:48:46 -0700

    removed imports

--- src/main/java/frc/robot/subsystems/HopperSubsystem/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem/HopperSubsystem.java
@@ -1,13 +1,7 @@
 package frc.robot.subsystems.HopperSubsystem;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
