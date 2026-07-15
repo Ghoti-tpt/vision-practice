62628953fabef96a2a66919ff3d9e26e4eb25f0d
pie850 <164816080+pie850@users.noreply.github.com>
2026-07-15 11:03:51 -0700

    added stator and supply to motor config

--- src/main/java/frc/robot/subsystems/HopperSubsystem/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem/HopperConstants.java
@@ -18,8 +18,17 @@ public class HopperConstants {
     private void configureMotor() {
         TalonFXConfiguration motorConfig = new TalonFXConfiguration();
 
-        motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motorConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
         // motorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+        // stator -> what your giving the motor
+        // supply -> what your 
+
+        motorConfig.CurrentLimits.StatorCurrentLimit = 80;
+        motorConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        motorConfig.CurrentLimits.SupplyCurrentLimit = 30;
+        motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        motorConfig.CurrentLimits.SupplyCurrentLowerLimit = 0;
         
         mHopperFx.getConfigurator().apply(motorConfig);
         mHopperFx.setControl(new Follower(mSideSweaperFx.getDeviceID(), MotorAlignmentValue.Opposed));
